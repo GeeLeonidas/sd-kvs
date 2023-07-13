fa6d07930630b13dd0e4eb4b4449b14e45fc5b43
Guilherme Leoi <geeleonidas@pm.me>
2023-07-12 22:17:29 -0300

    Add message field declaration

--- src/main/java/br/dev/gee/Mensagem.java
+++ src/main/java/br/dev/gee/Mensagem.java
@@ -3,5 +3,20 @@ package br.dev.gee;
 import java.io.Serializable;
 
 public class Mensagem implements Serializable {
-    // TODO: Message field declaration
+    public enum Code {
+        PUT, PUT_OK,
+        REPLICATION, REPLICATION_OK,
+        GET, TRY_OTHER_SERVER_OR_LATER
+    }
+
+    final Code code;
+    final String key, value;
+    final Long timestamp;
+
+    public Mensagem(Code code, String key, String value, long timestamp) {
+        this.code = code;
+        this.key = key;
+        this.value = value;
+        this.timestamp = timestamp;
+    }
 }
