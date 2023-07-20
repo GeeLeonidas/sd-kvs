8a54866d530d4d897b7a239af9366d54714306cc
Guilherme Leoi <geeleonidas@pm.me>
2023-07-19 21:08:25 -0300

    Fix `readPort` default behaviour

--- src/main/java/br/dev/gee/Servidor.java
+++ src/main/java/br/dev/gee/Servidor.java
@@ -74,6 +74,8 @@ public class Servidor {
         while (true) {
             System.out.printf("Insira a porta do %s (padrão %s): ", caller, DEFAULT_PORT);
             final String input = scanner.nextLine();
+            if (input.equals(""))
+                return DEFAULT_PORT;
             // Portas válidas (Fonte: https://ihateregex.io/expr/port)
             if (input.matches("^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$"))
                 return Integer.parseInt(input);
